800a88e82807fd9616038a632bee72b08e712a34
niilka <daemon.named@gmail.com>
2017-10-27 12:34:57 +0300

    big progress bars

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -11,3 +11,23 @@ $('.left-bar-collapse').click(function () {
     $('.links').removeClass('large');
   }
 });
+
+$('.top-bar-collapse').click(function () {
+  if ($('.top-bar').hasClass('is-collapsed')) {
+    $('.top-bar').removeClass('is-collapsed');
+    $('.top-bar').addClass('is-large');
+  } else {
+    $('.top-bar').addClass('is-collapsed');
+    $('.top-bar').removeClass('is-large');
+  }
+});
+
+$('.chat-bar-collapse').click(function () {
+  if ($('.chat-bar').hasClass('is-collapsed')) {
+    $('.chat-bar').removeClass('is-collapsed');
+    $('.chat-bar').addClass('is-large');
+  } else {
+    $('.chat-bar').addClass('is-collapsed');
+    $('.chat-bar').removeClass('is-large');
+  }
+});
